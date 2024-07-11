aac27fd0605c2c6c64b03c6895a8293f4b4719a0
Andrés Lázaro <aslazaro@comfenalcosantander.com.co>
2024-07-11 14:07:29 -0500

    proyecto de prueba

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="raiz">
+    <HelloWorld />
+  </div>
+  <div class="prueba">Hola Prueba</div>
+</template>
+<style scoped>
+.raiz {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+}
+
+.prueba {
+  z-index: 1;
+  color: aliceblue;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { shallowRef } from "vue";
+import { TresCanvas, useRenderLoop } from "@tresjs/core";
+import { OrbitControls, Stars } from "@tresjs/cientos";
+
+const yRotation = shallowRef(0);
+useRenderLoop().onLoop(({ delta }) => {
+  yRotation.value += 0.02 * delta;
+});
+</script>
+
+<template>
+  <TresCanvas class="color">
+    <TresPerspectiveCamera />
+    <Stars
+      :rotation="[0, yRotation, 0]"
+      :radius="50"
+      :depth="50"
+      :count="5000"
+      :size="0.3"
+      :size-attenuation="true"
+    />
+  </TresCanvas>
+</template>
+<style scoped>
+.color {
+  background-color: #222;
+}
+</style>
